382ebc947775bab2d728e8ce99c9d5e2dd6e2948
Sohez <57430072+sohez@users.noreply.github.com>
2022-03-12 19:51:12 +0530

    Create anim.js

---
+++ anim.js
@@ -0,0 +1,7 @@
+/* ============================== Code By sohel ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Linux User","Blogger","Web Developer","Android Developer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
