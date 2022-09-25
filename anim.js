41ace50daefe213d34439e80ecfdffa0bb819eb4
Sohez <57430072+sohez@users.noreply.github.com>
2022-09-25 22:26:42 +0530

    Delete anim.js

--- anim.js
+++
@@ -1,7 +0,0 @@
-/* ============================== Code By sohel ============================ */
-var typed = new Typed(".typing",{
-    strings:["","Web Designer","Linux User","Blogger","Web Developer","Android Developer","YouTuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
