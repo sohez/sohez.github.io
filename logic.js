132f2188002f2b9b6973cf8df920487b0999fa82
Sohez <57430072+sohez@users.noreply.github.com>
2023-05-03 15:43:45 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -154,29 +154,3 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
-
-//cursor efeeects
-    const cursor = document.querySelector(".cursor");
-    var timeout;
-
-    //follow cursor on mousemove
-    document.addEventListener("mousemove", (e) => {
-      let x = e.pageX;
-      let y = e.pageY;
-
-      cursor.style.top = y + "px";
-      cursor.style.left = x + "px";
-      cursor.style.display = "block";
-
-      //cursor effects when mouse stopped
-      function mouseStopped(){
-        cursor.style.display = "none";
-      }
-      clearTimeout(timeout);
-      timeout = setTimeout(mouseStopped, 8000);
-    });
-
-    //cursor effects when mouseout
-    document.addEventListener("mouseout", () => {
-      cursor.style.display = "none";
-    });
