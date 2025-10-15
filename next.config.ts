c1e62978c715c88c34ea7ea0bb71813760bd6e19
sohez <sohelsayyed1235@gmail.com>
2025-10-15 17:25:31 +0530

    chore: static genration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // → ensures pure static HTML/CSS/JS
+  images: { unoptimized: true }, // optional, prevents image optimization issues on GitHub Pages
+  basePath: "", // keep empty for root deployment
 };
 
 export default nextConfig;
