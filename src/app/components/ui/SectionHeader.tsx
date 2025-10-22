42a5225de4dac7cf9b07aebc607ebcfcf42cfd2d
sohez <sohelsayyed1235@gmail.com>
2025-10-22 19:30:43 +0530

    chore: text style chnaged

--- src/app/components/ui/SectionHeader.tsx
+++ src/app/components/ui/SectionHeader.tsx
@@ -19,7 +19,7 @@ const SectionHeader = ({
       <span className="underline decoration-1 decoration-red-500">
         {subtitle}
       </span>
-      <h2 className="font-display tracking-wide text-4xl font-bold text-gray-50">
+      <h2 className="font-display tracking-wide text-4xl font-medium text-gray-50">
         {title}
       </h2>
       <p className="text-gray-500">{description}</p>
