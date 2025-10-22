f9b7a40c482ba6d9a096bfb25ac3af9f621548b3
sohez <sohelsayyed1235@gmail.com>
2025-10-22 17:29:30 +0530

    chore: hero section updated

--- src/app/components/home/Hero.tsx
+++ src/app/components/home/Hero.tsx
@@ -1,7 +1,25 @@
-import { InstagramIcon, MailIcon, SendIcon } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import "./hero.css";
+import {
+  BsInstagram,
+  BsWhatsapp,
+} from "react-icons/bs";
+import { MdMailOutline } from "react-icons/md";
+
+const socialLinks = [
+  {
+    name: "Instagram",
+    icon: <BsInstagram size={20}/>,
+    url: "https://www.instagram.com/sohel_sayyed.dev",
+  },
+  { name: "Email", icon: <MdMailOutline size={25}/>, url: "mailto:techsohez@gmail.com" },
+  {
+    name: "WhatsApp",
+    icon: <BsWhatsapp size={20}/>,
+    url: "https://wa.me/+917887614788",
+  },
+];
 
 const Hero = () => {
   return (
@@ -15,38 +33,24 @@ const Hero = () => {
             className="grayscale-100 hover:grayscale-0 object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
           />
         </div>
-        <div className="flex flex-col justify-center items-center gap-4 max-w-3xl">
-
-          <div>
+        <div className="flex flex-col justify-center items-center mt-6 gap-8 md:gap-6 max-w-3xl">
+          <div className="flex flex-col justify-center items-center text-center md:text-start md:justify-start md:items-start">
             <span>Hi, I&apos;m</span>
-            <h1 className="font-display tracking-wide text-5xl font-bold text-gray-50">
+            <h1 className="font-display tracking-wide text-5xl font-medium text-gray-50">
               Sohel Sayyed
             </h1>
           </div>
-          <SkillsSlider />
-          <div>
-            <ul className="flex gap-3 justify-center items-center">
-              <li className="hover:text-gray-50 hover:border-b-[0.5px] p-1">
-                <InstagramIcon />
-              </li>
-              <li className="hover:text-gray-50 hover:border-b-[0.5px] p-1">
-                <MailIcon />
-              </li>
-              <li className="hover:text-gray-50 hover:border-b-[0.5px] p-1">
-                <SendIcon />
-              </li>
-            </ul>
-          </div>
 
-          <p className="text-lg">
+          <SocialLinks />
+          <SkillsSlider />
+
+          <p className="text-base tracking-wider text-center md:text-left">
             I&apos;m Sayyed Sohel, a professional with extensive experience in
             <span>crafting user-friendly websites </span>and powerful{" "}
             <span>Android applications</span>. I can help you bring your digital
             ideas to life, whether it&apos;s a sleek and modern website, a
             feature-rich mobile app, or both!
           </p>
-
-          {/* <SkillsSlider /> */}
         </div>
       </div>
     </section>
@@ -55,7 +59,6 @@ const Hero = () => {
 
 export default Hero;
 
-
 // slider style come from hero.css file.
 const SkillsSlider = () => {
   return (
@@ -63,11 +66,40 @@ const SkillsSlider = () => {
       {/* <div>I am</div> */}
       <div className="slider">
         <div className="slides ">
-          <div className="underline decoration-1 decoration-blue-500">Android Dev</div>
-          <div className="underline decoration-1 decoration-red-500">Fullstack Dev</div>
+          <div className="underline decoration-1 decoration-blue-500">
+            Android Dev
+          </div>
+          <div className="underline decoration-1 decoration-red-500">
+            Fullstack Dev
+          </div>
         </div>
       </div>
     </div>
   );
 };
-  
+
+const SocialLinks = () => {
+  return (
+    <div>
+      <ul className="flex gap-3 justify-center items-center">
+        {socialLinks.map((link) => {
+          return (
+            <li
+              key={link.name}
+              className="text-gray-500 hover:text-foreground border-red-500 hover:border-b-[0.5px] p-1"
+            >
+              <a
+                title={link.name}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link.icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
