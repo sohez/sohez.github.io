e4dca8eabf193df2add7620fdcbd3d64b00574e7
sohez <sohelsayyed1235@gmail.com>
2025-10-22 16:46:57 +0530

    feat: sction header added

---
+++ src/app/components/ui/SectionHeader.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+
+const SectionHeader = ({
+  title,
+  subtitle,
+  description,
+  className,
+}: {
+  className?: string,
+  title: string,
+  subtitle?: string,
+  description?: string,
+}) => {
+  const styles = `text-center flex flex-col justify-center items-center ${className}`;
+
+  return (
+    <div className={styles}>
+      <span className="underline decoration-1 decoration-red-500">
+        {subtitle}
+      </span>
+      <h2 className="font-display tracking-wide text-4xl font-bold text-gray-50">
+        {title}
+      </h2>
+      <p className="text-gray-500">{description}</p>
+    </div>
+  );
+};
+
+export default SectionHeader;
