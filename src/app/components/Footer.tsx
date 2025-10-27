d5c58ebb00588b4316682b765251d5bef3672a1c
sohez <sohelsayyed1235@gmail.com>
2025-10-27 19:31:39 +0530

    chore: social links fix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,20 +1,50 @@
 "use client";
-import { Code2Icon, InstagramIcon, SendIcon } from "lucide-react";
+import { Code2Icon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import { BsWhatsapp } from "react-icons/bs";
+import { BsInstagram, BsWhatsapp } from "react-icons/bs";
+import { FiLinkedin } from "react-icons/fi";
 import { GoMoveToTop } from "react-icons/go";
+import { MdMailOutline } from "react-icons/md";
+
+const socialLinks = [
+  {
+    name: "Instagram",
+    icon: <BsInstagram size={20} />,
+    url: "https://www.instagram.com/sohel_sayyed.dev",
+  },
+  {
+    name: "WhatsApp",
+    icon: <BsWhatsapp size={20} />,
+    url: "https://wa.me/+917887614788",
+  },
+  {
+    name: "Email",
+    icon: <MdMailOutline size={24} />,
+    url: "mailto:techsohez@gmail.com",
+  },
+  {
+    name: "Linkedin",
+    icon: <FiLinkedin size={24} />,
+    url: "https://www.linkedin.com/in/sohez/",
+  },
+];
+
+const handleMoveTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
 const Footer = () => {
   return (
     <footer className="w-full mt-35 bg-[#0D1117] p-4 shadow-[0_-10px_30px_rgba(13,17,23,0.45)] pb-14 pt-14">
       <div className="max-w-7xl mx-auto flex flex-col gap-3 justify-center items-center px-6 mb-3">
         <button
+          onClick={handleMoveTop}
           title="go to top"
           type="button"
           className="flex items-center gap-2 hover:scale-105 text-gray-500 hover:text-gray-200 transition-all duration-200 bg-gradient-to-b from-transparent to-transparent hover:from-transparent hover:to-gray-900 hover:border-b-[0.2] border-gray-500 px-2 py-1 rounded-lg"
         >
-          <GoMoveToTop size={24} className="" />
+          <GoMoveToTop size={24} />
         </button>
         <div className="mt-4 w-full flex flex-col-reverse md:flex-row gap-8 justify-between">
           <div className="flex gap-1 flex-col items-center md:items-start justify-center">
@@ -29,30 +59,17 @@ const Footer = () => {
 
           <div>
             <ul className="flex gap-2 items-center md:items-start justify-center">
-              <li>
-                <Link
-                  href="/"
-                  className="flex items-center gap-2  text-gray-500 hover:text-gray-200 transition-all duration-200 bg-gradient-to-b from-transparent to-transparent hover:from-transparent hover:to-gray-900 hover:border-b-[0.2] border-gray-500 px-3 py-2 rounded-lg"
-                >
-                  <InstagramIcon size={18} />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="flex items-center gap-2  text-gray-500 hover:text-gray-200 transition-all duration-200 bg-gradient-to-b from-transparent to-transparent hover:from-transparent hover:to-gray-900 hover:border-b-[0.2] border-gray-500 px-3 py-2 rounded-lg"
-                >
-                  <SendIcon size={18} />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="flex items-center gap-2  text-gray-500 hover:text-gray-200 transition-all duration-200 bg-gradient-to-b from-transparent to-transparent hover:from-transparent hover:to-gray-900 hover:border-b-[0.2] border-gray-500 px-3 py-2 rounded-lg"
-                >
-                  <BsWhatsapp size={18} />
-                </Link>
-              </li>
+              {socialLinks.map((link) => (
+                <li key={link.name}>
+                  <Link
+                    title={link.name}
+                    href={link.url}
+                    className="flex items-center justify-center gap-2  text-gray-500 hover:text-gray-200 transition-all duration-200 bg-gradient-to-b from-transparent to-transparent hover:from-transparent hover:to-gray-900 hover:border-b-[0.2] border-gray-500 px-3 py-2 rounded-lg"
+                  >
+                    {link.icon}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
