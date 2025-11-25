8a55f756e142f1c2d3a7917571690643d186e284
sohez <sohelsayyed1235@gmail.com>
2025-11-25 21:35:09 +0530

    - some ui changes.

--- src/app/components/home/Hero.tsx
+++ src/app/components/home/Hero.tsx
@@ -1,24 +1,25 @@
-'use client';
+"use client";
 import Image from "next/image";
 import React from "react";
 import "./hero.css";
-import {
-  BsInstagram,
-  BsWhatsapp,
-} from "react-icons/bs";
+import { BsInstagram, BsWhatsapp } from "react-icons/bs";
 import { MdMailOutline } from "react-icons/md";
 import { motion } from "framer-motion";
 
 const socialLinks = [
   {
     name: "Instagram",
-    icon: <BsInstagram size={20}/>,
+    icon: <BsInstagram size={20} />,
     url: "https://www.instagram.com/sohel_sayyed.0x",
   },
-  { name: "Email", icon: <MdMailOutline size={25}/>, url: "mailto:techsohez@gmail.com" },
+  {
+    name: "Email",
+    icon: <MdMailOutline size={25} />,
+    url: "mailto:techsohez@gmail.com",
+  },
   {
     name: "WhatsApp",
-    icon: <BsWhatsapp size={20}/>,
+    icon: <BsWhatsapp size={20} />,
     url: "https://wa.me/+917887614788",
   },
 ];
@@ -27,13 +28,12 @@ const Hero = () => {
   return (
     <section className="w-full mt-35 px-6">
       <div className="max-w-7xl mx-auto flex flex-col gap-2 justify-center items-center">
-        
         {/* Profile Image */}
         <motion.div
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="group relative w-40 sm:w-40 md:w-48 lg:w-56 aspect-square overflow-hidden rounded-full transition-all duration-300"
+          className="border-4 border-gray-800 group relative w-40 sm:w-40 md:w-48 lg:w-56 aspect-square overflow-hidden rounded-full transition-all duration-300"
         >
           <Image
             src="/images/hero/sohel-sayyed-image.webp"
@@ -51,19 +51,11 @@ const Hero = () => {
           className="flex flex-col justify-center items-center mt-6 gap-8 md:gap-6 max-w-3xl"
         >
           <div className="flex flex-col justify-center items-center text-center md:text-start md:justify-start md:items-start">
-            <span>Hi, I&apos;m</span>
-            <h1 className="font-display tracking-wide text-5xl font-medium text-gray-50">
+            <span>Hi, I'm</span>
+            <h1 className="font-display tracking-normal text-5xl font-medium text-gray-50">
               Sohel Sayyed
             </h1>
           </div>
-
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.5 }}
-          >
-            <SocialLinks />
-          </motion.div>
 
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -73,19 +65,26 @@ const Hero = () => {
             <SkillsSlider />
           </motion.div>
 
-          <p className="text-base tracking-wider text-center md:text-left">
+          <p className="text-xl tracking-wider text-center leading-relaxed">
             I&apos;m Sayyed Sohel, a professional with extensive experience in{" "}
-            <span>crafting user-friendly websites</span> and powerful{" "}
-            <span>Android applications</span>. I can help you bring your digital
+            <span>crafting user-friendly <span className="bg-blue-900/60 px-1">websites</span></span> and powerful{" "}
+            <span className="bg-purple-900/60 px-1">Android applications</span>. I can help you bring your digital
             ideas to life — whether it&apos;s a sleek modern website or a
             feature-rich mobile app!
           </p>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          >
+            <SocialLinks />
+          </motion.div>
         </motion.div>
       </div>
     </section>
   );
 };
-
 
 export default Hero;
 
@@ -93,13 +92,13 @@ export default Hero;
 const SkillsSlider = () => {
   return (
     <div className="flex gap-2 justify-center items-baseline">
-      {/* <div>I am</div> */}
+      <div>I'm</div>
       <div className="slider">
-        <div className="slides ">
-          <div className="underline decoration-1 decoration-blue-500">
+        <div className="slides text-gray-300">
+          <div>
             Android Dev
           </div>
-          <div className="underline decoration-1 decoration-red-500">
+          <div>
             Fullstack Dev
           </div>
         </div>
@@ -111,12 +110,12 @@ const SkillsSlider = () => {
 const SocialLinks = () => {
   return (
     <div>
-      <ul className="flex gap-3 justify-center items-center">
+      <ul className="flex gap-5 justify-center items-center">
         {socialLinks.map((link) => {
           return (
             <li
               key={link.name}
-              className="text-gray-500 hover:text-foreground border-red-500 hover:border-b-[0.5px] p-1"
+              className="transition-all duration-200 text-gray-400 hover:rotate-6 hover:scale-110 hover:text-gray-50"
             >
               <a
                 title={link.name}
